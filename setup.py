a6f670477fb8b83956c48f943f4f7edfe68f77cf
Ro6ertWcislo <robert.wcislo1995@gmail.com>
2017-10-30 10:39:14 +0100

    possibly made it pippable

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='gauss_jordan',
+      version='0.1',
+      description='Gauss-Jordan method and LU factorisation',
+      url='https://github.com/Ro6ertWcislo/GaussJordan',
+      license='MIT',
+      packages=['gauss_jordan'],
+      zip_safe=False)
